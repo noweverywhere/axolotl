7341aa84fe2e79f5b2c04d698e860b03054293d2
noweverywhere <marinus@noweverywhere.ca>
2017-05-13 20:44:32 +1000

    Remove magic numbers configuration of PIXI.js

--- app/index.js
+++ app/index.js
@@ -3,7 +3,18 @@ import store from './store'
 import { APP_LOADED } from './actions/app'
 import './lib/animatedSprite'
 
-let app = new global.PIXI.Application(400, 400, {backgroundColor: 0xff99bb})
+const options = {
+  width: 400,
+  height: 400,
+  additionalOptions: {
+    backgroundColor: 0xff99bb
+  }
+}
+let app = new global.PIXI.Application(
+  options.width,
+  options.height,
+  options.additionalOptions
+)
 
 document.body.appendChild(app.view)
 
